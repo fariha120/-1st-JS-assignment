52c1156acf659226ce1fb9af4b9987516c594c0b
fariha120 <156829302+fariha120@users.noreply.github.com>
2024-05-25 20:03:12 +0500

    Update script4.js
    added

--- script4.js
+++ script4.js
@@ -5,6 +5,8 @@ alert(num)
 alert(num2)
 alert(num3)
 
+document.write('<h2>Rules for naming JS variables</h2>')
+
 document.write("Variable names can only contain , numbers, $ and _ . for example: $my_1st variable <br>");
 document.write("Varible must begin with a latter, $ or_ for example : $name,_name or name<br>");
 document.write("Varible names are case sensitive <br>")
